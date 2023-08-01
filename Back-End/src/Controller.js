bf404d0a33ef5e43728094c728d681345ad4bc51
João Paulo Gallo <joaogallop@gmail.com>
2023-08-01 18:01:54 -0300

    add: node back-end

---
+++ Back-End/src/Controller.js
@@ -0,0 +1,85 @@
+const service = require('./Service')
+
+const getAll = async (req, res) => {
+    let json = { error: '', result: [] };
+    let users = await service.getAll();
+
+    for (let i in users) {
+        json.result.push({
+            id: users[i].id,
+            nome: users[i].nome,
+            dataNascimento: users[i].dataNascimento,
+            cpf: users[i].cpf,
+            telefone: users[i].telefone,
+        })
+    }
+    res.json(json);
+}
+
+const getOne = async (req, res) => {
+    let json = { error: '', result: {} };
+    let nome = req.params.nome;
+    let user = await service.getOne(nome);
+    if (user) {
+        json.result = user;
+    }
+    res.json(json);
+}
+
+const register = async (req, res) => {
+    let json = { error: '', result: {} };
+    //let id = req.body.id;
+    let nome = req.body.nome;
+    let dataNascimento = req.body.dataNascimento;
+    let cpf = req.body.cpf;
+    let telefone = req.body.telefone;
+    console.log(nome, dataNascimento, cpf, telefone);
+    if (nome && dataNascimento && cpf && telefone) {
+        let newUser = await service.register(nome, dataNascimento, cpf, telefone);
+        json.result = {
+            nome,
+            dataNascimento,
+            cpf,
+            telefone
+        };
+    } else {
+        json.error = 'Campos não enviados';
+    }
+    res.json(json);
+}
+
+const update = async (req, res) => {
+    let json = { error: '', result: {} };
+    let nome = req.body.nome;
+    let dataNascimento = req.body.dataNascimento;
+    let cpf = req.body.cpf;
+    let telefone = req.body.telefone;
+    let param = req.params.nome
+    if (nome && dataNascimento && cpf && telefone) {
+        await service.update(nome, dataNascimento, cpf, telefone, param);
+        json.result = {
+            nome,
+            dataNascimento,
+            cpf,
+            telefone
+        };
+    } else {
+        json.error = 'Campos não enviados';
+    }
+    res.json(json);
+}
+
+const erase = async (req, res) => {
+    let json = { error: '', result: {} };
+    await service.erase(req.params.id);
+    res.json(json);
+}
+
+
+module.exports = {
+    getAll,
+    getOne,
+    register,
+    update,
+    erase
+}
---
+++ Back-End/src/Service.js
@@ -0,0 +1,65 @@
+const db = require('./db')
+
+const getAll = () => {
+    return new Promise((accepted, rejected) => {
+        db.query('SELECT * FROM testNodeAngular', (error, results) => {
+            if (error) { rejected(error); return; }
+            accepted(results);
+        })
+    })
+}
+
+const getOne = (id) => {
+    return new Promise((accepted, rejected) => {
+
+        db.query('SELECT * FROM users WHERE id = ?', [id], (error, results) => {
+            if (error) { rejected(error); return; }
+            if (results.length > 0) { //vai verificar se retornou mais de 1 e pegar o 1
+                accepted(results[0]);
+            } else {
+                accepted(false);
+            }
+        });
+    });
+}
+const register = (nome, dataNascimento, cpf, telefone) => {
+    return new Promise((accepted, rejected) => {
+
+        db.query('INSERT INTO testNodeAngular (nome, dataNascimento, cpf, telefone) VALUES ( ?, ?, ?, ?)',
+            [nome, dataNascimento, cpf, telefone],
+            (error, results) => {
+                if (error) { rejected(error); return; }
+                accepted(results);
+            }
+        );
+    });
+}
+const update = (nome, dataNascimento, cpf, telefone, param) => {
+    return new Promise((accepted, rejected) => {
+        db.query('UPDATE testNodeAngular SET nome = ?, dataNascimento = ?, cpf = ?, telefone = ? WHERE nome = ?',
+            [nome, dataNascimento, cpf, telefone, param],
+            (error, results) => {
+                if (error) { rejected(error); return; }
+                accepted(results);
+            }
+        );
+    });
+}
+
+const erase = (id) => {
+    return new Promise((accepted, rejected) => {
+        db.query('DELETE FROM testNodeAngular WHERE id = ?', [id], (error, results) => {
+            if (error) { rejected(error); return; }
+            accepted(results);
+        });
+    });
+}
+
+
+module.exports = {
+    getAll,
+    getOne,
+    register,
+    update,
+    erase
+}
